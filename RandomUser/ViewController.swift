09f1bc8c81479b7045f79fe7f7254992599701dc
Stephen Furlani <stephen.furlani@gmail.com>
2021-09-12 22:31:07 -0400

    Initial Commit

---
+++ RandomUser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomUser
+//
+//  Created by Stephen Furlani on 9/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
